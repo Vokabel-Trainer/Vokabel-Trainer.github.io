4f0b419ccba21c1ac00e681bf9db87498b027e88
Helmut Bezolt <helmut.bezolt@gmail.com>
2023-05-19 23:32:41 +0200

    update colors

--- src/routes/play/SelectAnswer.svelte
+++ src/routes/play/SelectAnswer.svelte
@@ -8,6 +8,7 @@
 	export let onAnswer: ((correct: boolean) => Promise<void>) | null = null;
 
 	let correct: boolean | null = null;
+	let selectedAnswer: string | null = null;
 
 	$: {
 		correct = null;
@@ -16,7 +17,30 @@
 	}
 
 	async function nextPage() {
+		if (correct == null) {
+			correct = checkAnswer(selectedAnswer!);
+			return;
+		}
+
 		onAnswer && (await onAnswer(correct!));
+	}
+
+	function selectAnswer(answer: string) {
+		if (correct == null) {
+			selectedAnswer = answer;
+		}
+	}
+
+	function getButtonClass(answer: string, selectedAnswer: string | null, correct: boolean | null) {
+		if (correct == null) {
+			return selectedAnswer === answer ? 'btn-warning' : '';
+		}
+		if (correctAnswers.includes(answer)) {
+			return 'btn-success';
+		}
+		if (!correct) {
+			return selectedAnswer === answer ? 'btn-error' : '';
+		}
 	}
 </script>
 
@@ -26,29 +50,28 @@
 	<div class="flex flex-row w-full justify-center flex-wrap gap-1">
 		{#each possibleAnswers as answer}
 			<button
-				class={`btn ${correct != null && correctAnswers.includes(answer) ? 'btn-success' : ''}`}
-				on:click={() => (correct = checkAnswer(answer))}>{answer}</button
+				class={`btn ${getButtonClass(answer, selectedAnswer, correct)}`}
+				on:click={() => selectAnswer(answer)}>{answer}</button
 			>
 		{/each}
 	</div>
 
-	{#if correct != null}
-		{#if correct}
-			<div
-				class="overflow-hidden"
-				style="position: absolute; left: 50%; top: 30%"
-				use:confetti={{
-					particleCount: 50,
-					force: 0.7,
-					stageWidth: window.innerWidth / 2,
-					stageHeight: window.innerHeight / 2,
-					colors: ['#ff3e00', '#40b3ff', '#676778']
-				}}
-			/>
-		{/if}
-
-		<button class={`btn mt-2 w-64 ${correct ? 'btn-success' : 'btn-error'}`} on:click={nextPage}
-			>Weiter</button
-		>
+	<button
+		class={`btn mt-2 w-64 ${correct == null ? '' : correct ? 'btn-success' : 'btn-error'}`}
+		on:click={nextPage}
+		disabled={selectedAnswer == null}>Weiter</button
+	>
+	{#if correct}
+		<div
+			class="overflow-hidden"
+			style="position: absolute; left: 50%; top: 30%"
+			use:confetti={{
+				particleCount: 50,
+				force: 0.7,
+				stageWidth: window.innerWidth / 2,
+				stageHeight: window.innerHeight / 2,
+				colors: ['#ff3e00', '#40b3ff', '#676778']
+			}}
+		/>
 	{/if}
 </div>

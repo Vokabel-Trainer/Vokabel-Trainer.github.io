2c7b24d20134461b8acf14304953ac55c7779c8c
Helmut Bezolt <helmut.bezolt@gmail.com>
2023-05-20 16:28:40 +0200

    move buttons, use static svelte adapter, only include required themes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex-1 flex justify-center items-center flex-wrap gap-1 flex-col sm:flex-row">
 	<a class="btn" href="/play">Starten</a>
-	<a class="btn" href="/select-categories">Kategorien auswählen</a>
 	<a class="btn" href="/select-language">Sprache ändern</a>
+	<a class="btn" href="/select-categories">Kategorien auswählen</a>
 	<a class="btn" href="/settings">Einstellungen</a>
 </div>

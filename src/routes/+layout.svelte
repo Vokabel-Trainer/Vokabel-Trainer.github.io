f41a457709a9f7b1b6779e709a6644e820e32b63
Helmut Bezolt <helmut.bezolt@gmail.com>
2023-05-20 17:19:54 +0200

    add back button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
 	import { anyCategoriesSelected } from '$lib/db';
 	import { getName, getSelectedLanguage } from '$lib/languages';
 	import { anyCategoriesSelectedStore, languagePairStore } from '$lib/store';
@@ -11,16 +12,34 @@
 			anyCategoriesSelected().then(anyCategoriesSelectedStore.set);
 		}
 	}
+
+	$: showBackButton = browser && $page.route.id !== '/';
+
+	function handleBackClick() {
+		window.history.back();
+	}
 </script>
 
 <main class="p-1 flex h-full w-full flex-col">
 	{#if browser}
 		{#if $languagePairStore}
-			<p class="font-bold">
-				Ausgewählte Sprache: {getName($languagePairStore.from)} - {getName($languagePairStore.to)}
-			</p>
+			<div class="flex mx-1 mt-1">
+				<p class="font-bold flex-1">
+					Ausgewählte Sprache: {getName($languagePairStore.from)} - {getName($languagePairStore.to)}
+				</p>
+				<button
+					class={`btn btn-sm btn-circle ${showBackButton ? '' : 'hidden'}`}
+					on:click={handleBackClick}>X</button
+				>
+			</div>
 		{:else}
-			<p class="font-bold">Aktuell ist keine Sprache ausgewählt.</p>
+			<div class="flex mx-1 mt-1">
+				<p class="font-bold flex-1">Aktuell ist keine Sprache ausgewählt.</p>
+				<button
+					class={`btn btn-sm btn-circle ${showBackButton ? '' : 'hidden'}`}
+					on:click={handleBackClick}>X</button
+				>
+			</div>
 		{/if}
 	{:else}
 		<p class="font-bold">Ausgewählte Sprachen werden geladen.</p>

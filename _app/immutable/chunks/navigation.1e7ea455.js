63dc217ea863b620fb60446e7463756a33eab04f
Hummel37 <Hummel37@users.noreply.github.com>
2023-05-20 22:17:25 +0000

    deploy: 3b0ad7c8efa35c46738ce9bf160d20782a7b603f

---
+++ _app/immutable/chunks/navigation.1e7ea455.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.46461f8c.js";const e=o("goto");export{e as g};
--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  s + "/_app/immutable/entry/app.a40a988c.js",
+  s + "/_app/immutable/entry/app.845a7f7b.js",
   s + "/_app/immutable/assets/0.dab6b812.css",
-  s + "/_app/immutable/nodes/0.0a23880d.js",
-  s + "/_app/immutable/nodes/1.28b74139.js",
+  s + "/_app/immutable/nodes/0.8d32c5e1.js",
+  s + "/_app/immutable/nodes/1.830aa068.js",
   s + "/_app/immutable/nodes/2.6c330930.js",
   s + "/_app/immutable/nodes/3.f07dcaf8.js",
   s + "/_app/immutable/nodes/4.219d868b.js",
-  s + "/_app/immutable/nodes/5.b0fe3d19.js",
-  s + "/_app/immutable/nodes/6.2baca0f1.js",
+  s + "/_app/immutable/nodes/5.c40735cd.js",
+  s + "/_app/immutable/nodes/6.a429f1a1.js",
   s + "/_app/immutable/nodes/7.057b8c32.js",
   s + "/_app/immutable/chunks/db.c2c3de62.js",
   s + "/_app/immutable/chunks/index.9d9b8d81.js",
   s + "/_app/immutable/chunks/index.fd817f55.js",
   s + "/_app/immutable/chunks/languages.eb18cb82.js",
   s + "/_app/immutable/chunks/methods.63081a9d.js",
-  s + "/_app/immutable/chunks/navigation.7dd00a22.js",
-  s + "/_app/immutable/chunks/singletons.3acabaf3.js",
+  s + "/_app/immutable/chunks/navigation.1e7ea455.js",
+  s + "/_app/immutable/chunks/singletons.46461f8c.js",
   s + "/_app/immutable/chunks/store.47891479.js",
-  s + "/_app/immutable/chunks/stores.3e602953.js",
-  s + "/_app/immutable/entry/start.0b8ca647.js"
+  s + "/_app/immutable/chunks/stores.fa9ad30f.js",
+  s + "/_app/immutable/entry/start.63d14259.js"
 ], d = [
   s + "/android-chrome-192x192.png",
   s + "/android-chrome-512x512.png",
@@ -37,7 +37,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r
   s + "/site.webmanifest",
   s + "/success.mp3",
   s + "/wrong.mp3"
-], i = "1684620972655", n = self, p = `cache${i}`, m = r.concat(d), h = new Set(m);
+], i = "1684621037637", n = self, p = `cache${i}`, m = r.concat(d), h = new Set(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(p).then((a) => a.addAll(m)).then(() => {

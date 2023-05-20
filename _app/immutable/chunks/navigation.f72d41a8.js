ea56eb8780475e39bc404cfb3ecbdb09de23bc7e
Hummel37 <Hummel37@users.noreply.github.com>
2023-05-20 22:43:10 +0000

    deploy: 9e4ab43d6e3c2becccad913066872686f47f14f0

---
+++ _app/immutable/chunks/navigation.f72d41a8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.6f045f52.js";const e=o("goto");export{e as g};
--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  s + "/_app/immutable/entry/app.5a7653c7.js",
+  s + "/_app/immutable/entry/app.3a5fa6ab.js",
   s + "/_app/immutable/assets/0.dab6b812.css",
-  s + "/_app/immutable/nodes/0.502232a6.js",
-  s + "/_app/immutable/nodes/1.9f9aa61c.js",
+  s + "/_app/immutable/nodes/0.ab772adb.js",
+  s + "/_app/immutable/nodes/1.bb024d9b.js",
   s + "/_app/immutable/nodes/2.6c330930.js",
   s + "/_app/immutable/nodes/3.4da40cba.js",
   s + "/_app/immutable/nodes/4.219d868b.js",
-  s + "/_app/immutable/nodes/5.12dc106e.js",
-  s + "/_app/immutable/nodes/6.77712062.js",
+  s + "/_app/immutable/nodes/5.ef6516b4.js",
+  s + "/_app/immutable/nodes/6.dc02c15e.js",
   s + "/_app/immutable/nodes/7.057b8c32.js",
   s + "/_app/immutable/chunks/db.c2c3de62.js",
   s + "/_app/immutable/chunks/index.9d9b8d81.js",
   s + "/_app/immutable/chunks/index.fd817f55.js",
   s + "/_app/immutable/chunks/languages.eb18cb82.js",
   s + "/_app/immutable/chunks/methods.63081a9d.js",
-  s + "/_app/immutable/chunks/navigation.892e096b.js",
-  s + "/_app/immutable/chunks/singletons.8a56cac2.js",
+  s + "/_app/immutable/chunks/navigation.f72d41a8.js",
+  s + "/_app/immutable/chunks/singletons.6f045f52.js",
   s + "/_app/immutable/chunks/store.47891479.js",
-  s + "/_app/immutable/chunks/stores.2f48a537.js",
-  s + "/_app/immutable/entry/start.44927871.js"
-], h = [
+  s + "/_app/immutable/chunks/stores.e89a1012.js",
+  s + "/_app/immutable/entry/start.c4873753.js"
+], d = [
   s + "/android-chrome-192x192.png",
   s + "/android-chrome-512x512.png",
   s + "/apple-touch-icon.png",
@@ -37,7 +37,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r
   s + "/site.webmanifest",
   s + "/success.mp3",
   s + "/wrong.mp3"
-], i = "1684622272621", n = self, p = `cache${i}`, m = r.concat(h), u = new Set(m);
+], i = "1684622583464", n = self, p = `cache${i}`, m = r.concat(d), h = new Set(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(p).then((a) => a.addAll(m)).then(() => {
@@ -54,7 +54,7 @@ n.addEventListener("activate", (e) => {
     })
   );
 });
-async function d(e) {
+async function u(e) {
   const a = await caches.open(`offline${i}`);
   try {
     const t = await fetch(e);
@@ -69,8 +69,8 @@ async function d(e) {
 n.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET" || e.request.headers.has("range"))
     return;
-  const a = new URL(e.request.url), t = a.protocol.startsWith("http"), c = a.hostname === self.location.hostname && a.port !== self.location.port, o = a.host === self.location.host && u.has(a.pathname), l = e.request.cache === "only-if-cached" && !o;
+  const a = new URL(e.request.url), t = a.protocol.startsWith("http"), c = a.hostname === self.location.hostname && a.port !== self.location.port, o = a.host === self.location.host && h.has(a.pathname), l = e.request.cache === "only-if-cached" && !o;
   t && !c && !l && e.respondWith(
-    (async () => o && await caches.match(e.request) || d(e.request))()
+    (async () => o && await caches.match(e.request) || u(e.request))()
   );
 });

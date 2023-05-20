ad20fc1697d06b7fbe99009c8b884952577a237c
Helmut Bezolt <helmut.bezolt@gmail.com>
2023-05-20 16:50:52 +0200

    move methods back to the +layout.svelte file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
-	import { getName } from '$lib/languages';
-	import { languagePairStore } from '$lib/store';
+	import { browser } from '$app/environment';
+	import { anyCategoriesSelected } from '$lib/db';
+	import { getName, getSelectedLanguage } from '$lib/languages';
+	import { anyCategoriesSelectedStore, languagePairStore } from '$lib/store';
 	import '../app.css';
+
+	$: {
+		if (browser) {
+			languagePairStore.set(getSelectedLanguage());
+			anyCategoriesSelected().then(anyCategoriesSelectedStore.set);
+		}
+	}
 </script>
 
 <main class="p-1 flex h-full w-full flex-col">
-	{#if $languagePairStore}
-		<p class="font-bold">
-			Ausgewählte Sprache: {getName($languagePairStore.from)} - {getName($languagePairStore.to)}
-		</p>
+	{#if browser}
+		{#if $languagePairStore}
+			<p class="font-bold">
+				Ausgewählte Sprache: {getName($languagePairStore.from)} - {getName($languagePairStore.to)}
+			</p>
+		{/if}
+	{:else}
+		<p class="font-bold">Ausgewählte Sprachen werden geladen.</p>
 	{/if}
 	<slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
-	import { anyCategoriesSelected } from '$lib/db';
-	import { getSelectedLanguage } from '$lib/languages';
 	import { anyCategoriesSelectedStore, languagePairStore } from '$lib/store';
-
-	$: {
-		if (browser) {
-			languagePairStore.set(getSelectedLanguage());
-			anyCategoriesSelected().then(anyCategoriesSelectedStore.set);
-		}
-	}
 
 	$: langSelected = $languagePairStore != null;
 </script>
@@ -20,7 +10,9 @@
 </svelte:head>
 
 <div class="flex-1 flex justify-center items-center flex-wrap gap-1 flex-col sm:flex-row">
-	<a class={`btn ${langSelected && $anyCategoriesSelectedStore ? '' : 'btn-disabled'}`} href="/play">Starten</a>
+	<a class={`btn ${langSelected && $anyCategoriesSelectedStore ? '' : 'btn-disabled'}`} href="/play"
+		>Starten</a
+	>
 	<a class="btn" href="/select-language">Sprache ändern</a>
 	<a class={`btn ${langSelected ? '' : 'btn-disabled'}`} href="/select-categories"
 		>Kategorien auswählen</a
